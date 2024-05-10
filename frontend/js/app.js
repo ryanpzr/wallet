95834eae3be1f014c53980524f0035da8a3f8d7a
Ryan <ryanpereiralimads@gmail.com>
2024-05-10 17:00:55 -0300

    Exception in js folder created

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,48 +1,63 @@
 function enviarDadosParaBackend() {
-    // Obtendo o token JWT
-    var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ3YWxsZXR3aXphcmRfc2VydmljZSIsInN1YiI6InJ5YW5wZXJlaXJhbGltYWRzQGdtYWlsLmNvbSIsImV4cCI6MTcxNTMyOTUwNn0.pwWsiLS-xV6GU5q8iYWc9d7142ATmRgfRv-a1vhCNAk';
+    console.log("erro")
 
-    // Obtendo os valores dos campos do formulário
-    var nome = document.getElementById('nome').value;
-    var valor = document.getElementById('valor').value;
-    var resumo = document.getElementById('resumo').value;
-    var data = document.getElementById('data').value;
-    var categoria = document.getElementById('categoria').value;
+    try {
+        console.log("erro")
 
-    var dataParaExtracao = new Date(data);
-    var dataNumero = dataParaExtracao.getMonth() + 1;
+        // Obtendo o token JWT
+        var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ3YWxsZXR3aXphcmRfc2VydmljZSIsInN1YiI6InJ5YW5wZXJlaXJhbGltYWRzQGdtYWlsLmNvbSIsImV4cCI6MTcxNTM2MjI4OH0.F1NYekF-V4ILGxWuynOaAoNYUlJ-hVa_ioQ33a_9fW4';
 
-    // Criando um objeto com os dados a serem enviados
-    var dados = {
-        nomeCompra: nome,
-        valorCompra: valor,
-        descricao: resumo,
-        date: data,
-        category: categoria,
-        incomeId: dataNumero
-    };
+        // Obtendo os valores dos campos do formulário
+        var nome = document.getElementById('nome').value;
+        var valor = document.getElementById('valor').value;
+        var resumo = document.getElementById('resumo').value;
+        var data = document.getElementById('data').value;
+        var categoria = document.getElementById('categoria').value;
 
-    // Enviando os dados para o backend usando fetch
-    fetch('http://localhost:8080/api/expense', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token // Adicionando o token JWT ao cabeçalho de autorização
-        },
-        body: JSON.stringify(dados)
-    })
-        .then(response => {
-            if (response.ok) {
-                // Se a resposta do servidor for bem-sucedida
-                console.log('Dados enviados com sucesso!');
-            } else {
-                // Se a resposta do servidor não for bem-sucedida
-                console.error('Erro ao enviar os dados:', response.statusText);
-            }
+        var dataParaExtracao = new Date(data);
+        var dataNumero = dataParaExtracao.getMonth() + 1;
+
+        // Criando um objeto com os dados a serem enviados
+        var dados = {
+            nomeCompra: nome,
+            valorCompra: valor,
+            descricao: resumo,
+            date: data,
+            category: categoria,
+            incomeId: dataNumero
+        };
+
+        document.write(dados)
+
+        // Enviando os dados para o backend usando fetch
+        fetch('http://localhost:8080/api/expense', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token // Adicionando o token JWT ao cabeçalho de autorização
+            },
+            body: JSON.stringify(dados)
         })
-        .catch(error => {
-            console.error('Erro ao enviar os dados:', error
+            .then(response => {
+                if (response.ok) {
+                    // Se a resposta do servidor for bem-sucedida
+                    console.log('Dados enviados com sucesso!');
+                } else {
+                    // Se a resposta do servidor não for bem-sucedida
+                    console.error('Erro ao enviar os dados:', response.statusText);
+                }
+            })
+            .catch(error => {
+                console.error('Erro ao enviar os dados:', error
 
-            );
-        });
+                );
+            });
+
+    } catch (error) {
+        console.error('Erro ao enviar os dados para o backend:', error);
+    }
+
 }
+
+
+
